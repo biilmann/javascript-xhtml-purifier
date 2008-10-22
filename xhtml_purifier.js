a6112f3cb8caa10c21b47c4df3cf620930ee1b0a
Mathias Biilmann Christensen <mathiasch@webpop.com>
2008-10-22 09:33:09 +0200

    Added brief copyright message

--- xhtml_purifier.js
+++ xhtml_purifier.js
@@ -1,25 +1,10 @@
 /*
+ * XHTML Purifier By Mathias Biilmann Christensen
+ * Copyright Domestika 2008
+ *
  * HTML Parser By John Resig (ejohn.org)
  * Original code by Erik Arvidsson, Mozilla Public License
  * http://erik.eae.net/simplehtmlparser/simplehtmlparser.js
- *
- * // Use like so:
- * HTMLParser(htmlString, {
- *     start: function(tag, attrs, unary) {},
- *     end: function(tag) {},
- *     chars: function(text) {},
- *     comment: function(text) {}
- * });
- *
- * // or to get an XML string:
- * HTMLtoXML(htmlString);
- *
- * // or to get an XML DOM Document
- * HTMLtoDOM(htmlString);
- *
- * // or to inject into an existing document/DOM node
- * HTMLtoDOM(htmlString, document);
- * HTMLtoDOM(htmlString, document.body);
  *
  */
 
